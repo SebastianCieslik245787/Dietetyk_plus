48936267f33c19aba0f71a99d8b71ea13e5cef1e
Seba <245787@edu.p.lodz.pl>
2025-05-14 15:49:24 +0200

    Few changes in User Settings

--- src/assets/elements/user_settings/UserSettingsLabel.jsx
+++ src/assets/elements/user_settings/UserSettingsLabel.jsx
@@ -1,11 +1,16 @@
-const UserSettingsInput = ({label}) => {
+const UserSettingsInput = ({labels, onClick, isActive}) => {
 
 
     return(
         <div className="user-settings-label">
-           <p className="user-settings-label-text">
-               {label}
-           </p>
+            {
+                labels.map((label, index) => (
+                    <p onClick={() => onClick(index)} key={index} className={`user-settings-label-text ${index === 0 ? "user-settings-label-text-first" : ""} ${isActive === index ? "user-settings-label-text-active" : ""}`}>
+                        {label}
+                    </p>
+                ))
+            }
+
         </div>
     );
 }; export default UserSettingsInput;
--- src/pages/UserSettings.jsx
+++ src/pages/UserSettings.jsx
@@ -9,6 +9,8 @@ import {useCookies} from "react-cookie"
 
 export function UserSettings() {
     const [cookies, setCookies] = useCookies(['User-Data']);
+    const [actualSettings, setActualSettings] = useState(0);
+
     const [userPasswordData, setUserPasswordData] = useState({
         userPassword: "",
         userConfirmPassword: "",
@@ -24,6 +26,7 @@ export function UserSettings() {
 
     const [userPersonalData, setUserPersonalData] = useState(initialData);
     const [isModifiedPersonalData, setIsModifiedPersonalData] = useState(false);
+    const [isModifiedDescription, setIsModifiedDescription] = useState(false);
 
     useEffect(() => {
         const dataChanged = JSON.stringify(userPersonalData) !== JSON.stringify(initialData);
@@ -36,83 +39,116 @@ export function UserSettings() {
             <div className="user-settings-container">
                 <div className="user-settings-personal-data">
                     <UserSettingsLabel
-                        label="Dane Personalne"
+                        labels={[
+                            "Dane Personalne",
+                            "Zmiana hasła",
+                            "Opis"
+                        ]}
+                        onClick={setActualSettings}
+                        isActive={actualSettings}
                     />
-                    <UserSettingsInput
-                        id="userName"
-                        label="Imię:"
-                        value={userPersonalData.userName}
-                        setFormData={setUserPersonalData}
-                        type="text"
-                    />
-                    <UserSettingsInput
-                        id="userSurname"
-                        placeHolder=""
-                        label="Nazwisko:"
-                        value={userPersonalData.userSurname}
-                        setFormData={setUserPersonalData}
-                        type="text"
-                    />
-                    <UserSettingsInput
-                        id="userEmail"
-                        placeHolder="Wpisz e-mail..."
-                        label="E-mail:"
-                        value={userPersonalData.userEmail}
-                        setFormData={setUserPersonalData}
-                        type="text"
-                        readOnly={false}
-                    />
-                    <UserSettingsInput
-                        id="userPhone"
-                        placeHolder="Wpisz nr telefonu..."
-                        label="Nr telefonu:"
-                        value={userPersonalData.userPhone}
-                        setFormData={setUserPersonalData}
-                        type="text"
-                        readOnly={false}
-                    />
-                    <div className={`user-settings-save-button ${isModifiedPersonalData ? "user-settings-save-button-active" : ""}`} onClick={()=>{changeUserData(setCookies, cookies, userPersonalData)}}>
-                        Zapisz
-                    </div>
-                </div>
-                <div className="user-settings-personal-data">
-                    <UserSettingsLabel
-                        label="Zmiana hasła"
-                    />
-                    <UserSettingsInput
-                        id="userPassword"
-                        label="Hasło:"
-                        placeHolder="Wpisz aktualne hasło..."
-                        value={userPasswordData.userPassword}
-                        setFormData={setUserPasswordData}
-                        type="password"
-                        readOnly={false}
-                    />
-                    <UserSettingsInput
-                        id="userConfirmPassword"
-                        placeHolder="Potwierdź hasło..."
-                        label="Potwierdz hasło:"
-                        value={userPasswordData.userConfirmPassword}
-                        setFormData={setUserPasswordData}
-                        type="password"
-                        readOnly={false}
-                    />
-                    <UserSettingsInput
-                        id="userNewPassword"
-                        placeHolder="Wpisz nowe hasło..."
-                        label="Nowe hasło:"
-                        value={userPasswordData.userNewPassword}
-                        setFormData={setUserPasswordData}
-                        type="password"
-                        readOnly={false}
-                    />
-                    <div className={`user-settings-save-button user-settings-save-button-active`} onClick={()=>{changeUserPassword(setCookies,cookies,userPasswordData)}}>
-                        Zapisz
-                    </div>
+                    {
+                        actualSettings === 0 && (
+                            <>
+                                <UserSettingsInput
+                                    id="userName"
+                                    label="Imię:"
+                                    value={userPersonalData.userName}
+                                    setFormData={setUserPersonalData}
+                                    type="text"
+                                />
+                                <UserSettingsInput
+                                    id="userSurname"
+                                    placeHolder=""
+                                    label="Nazwisko:"
+                                    value={userPersonalData.userSurname}
+                                    setFormData={setUserPersonalData}
+                                    type="text"
+                                />
+                                <UserSettingsInput
+                                    id="userEmail"
+                                    placeHolder="Wpisz e-mail..."
+                                    label="E-mail:"
+                                    value={userPersonalData.userEmail}
+                                    setFormData={setUserPersonalData}
+                                    type="text"
+                                    readOnly={false}
+                                />
+                                <UserSettingsInput
+                                    id="userPhone"
+                                    placeHolder="Wpisz nr telefonu..."
+                                    label="Nr telefonu:"
+                                    value={userPersonalData.userPhone}
+                                    setFormData={setUserPersonalData}
+                                    type="text"
+                                    readOnly={false}
+                                />
+                                <div
+                                    className={`user-settings-save-button ${isModifiedPersonalData ? "user-settings-save-button-active" : ""}`}
+                                    onClick={() => {
+                                        changeUserData(setCookies, cookies, userPersonalData)
+                                    }}>
+                                    Zapisz
+                                </div>
+                            </>
+                        )
+                    }
+                    {actualSettings === 1 && (
+                        <>
+                            <UserSettingsInput
+                                id="userPassword"
+                                label="Hasło:"
+                                placeHolder="Wpisz aktualne hasło..."
+                                value={userPasswordData.userPassword}
+                                setFormData={setUserPasswordData}
+                                type="password"
+                                readOnly={false}
+                            />
+                            <UserSettingsInput
+                                id="userConfirmPassword"
+                                placeHolder="Potwierdź hasło..."
+                                label="Potwierdz hasło:"
+                                value={userPasswordData.userConfirmPassword}
+                                setFormData={setUserPasswordData}
+                                type="password"
+                                readOnly={false}
+                            />
+                            <UserSettingsInput
+                                id="userNewPassword"
+                                placeHolder="Wpisz nowe hasło..."
+                                label="Nowe hasło:"
+                                value={userPasswordData.userNewPassword}
+                                setFormData={setUserPasswordData}
+                                type="password"
+                                readOnly={false}
+                            />
+                            <div className={`user-settings-save-button user-settings-save-button-active`}
+                                 onClick={() => {
+                                     changeUserPassword(setCookies, cookies, userPasswordData)
+                                 }}>
+                                Zapisz
+                            </div>
+                        </>
+                    )}
+                    {
+                        actualSettings === 2 && (
+                            <>
+                                <textarea className={"user-settings-textarea"} placeholder="Opisz siebie..."/>
+                                <div
+                                    className={`user-settings-save-button user-settings-save-button-active`}
+                                    onClick={() => {
+                                        changeUserData(setCookies, cookies, userPersonalData)
+                                    }}>
+                                    Zapisz
+                                </div>
+                            </>
+                        )
+                    }
                 </div>
             </div>
         </>
-    );
+    )
+        ;
 }
 
 export default UserSettings;
