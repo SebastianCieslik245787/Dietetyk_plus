253a4129c8c0dfae7bfd092d464147fd1ff090c3
Seba <245787@edu.p.lodz.pl>
2025-05-14 22:40:17 +0200

    Adding Logout page

--- src/pages/LogOutPage.jsx
+++ src/pages/LogOutPage.jsx
@@ -1,6 +1,25 @@
+import "../style/LogoutPage.css"
+import {useNavigate} from "react-router-dom";
+
 function LogOutPage(){
+    const navigate = useNavigate();
+
+    const handleBackToHomePageClick = () => {
+        navigate("/home");
+    }
+
     return (
         <>
+            <div className="logout-coinatiner">
+                <div className="logout-panel">
+                    <div className="logout-information">
+                        Pomyślnie Wylogowano...
+                    </div>
+                    <div className="logout-back-to-homepage">
+                        <p onClick={handleBackToHomePageClick}>Powrót do strony głównej</p>
+                    </div>
+                </div>
+            </div>
         </>
     );
 } export default LogOutPage;
