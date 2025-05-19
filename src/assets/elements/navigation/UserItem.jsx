8c24f72b3be06dc3fbfe8c411cb1807a1afad9f1
JuliuszZolich <245971@edu.p.lodz.pl>
2025-05-19 21:10:38 +0200

    Dynamic user icon on navbar

--- src/assets/elements/navigation/UserItem.jsx
+++ src/assets/elements/navigation/UserItem.jsx
@@ -1,10 +1,11 @@
 import UserIcon from "../../../images/icons/deafult_user_icon.png"
 import {useEffect, useRef, useState} from "react";
 import {useNavigate} from "react-router-dom";
+import {useCookies} from "react-cookie";
 
 const UserItem = () => {
     const navigate = useNavigate();
-
+    const [cookies] = useCookies(["User-Data"]);
     const onLogoutClick = () => {
         navigate("/logout");
     }
@@ -31,7 +32,7 @@ const UserItem = () => {
 
     return (
         <div className="user-item" ref={containerRef}>
-            <img src={UserIcon} alt="" onClick={() => setClicked(!isClicked)} />
+            <img src={cookies["User-Data"].img_b64 !== "" ? cookies["User-Data"].img_b64 : UserIcon} alt="" onClick={() => setClicked(!isClicked)} />
             <div className={`user-options ${isClicked ? "user-options-active" : ""}`}>
                 <div className="user-option" onClick={onSettingsClick}>
                     Ustawienia
