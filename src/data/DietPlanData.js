57898978d64b9d1108561cbb020fa38533b8cc1e
Seba <245787@edu.p.lodz.pl>
2025-05-21 17:23:37 +0200

    Fixing diet plan page

--- src/data/DietPlanData.js
+++ src/data/DietPlanData.js
@@ -15,3 +15,349 @@ export const mealNames = [
     "Podwieczorek",
     "Kolacja",
 ]
+
+export const dietPlanData = {
+    days: [
+        {
+            meals: [
+                {
+                    name: "Śniadanie",
+                    meal: {
+                        name: "Jajecznica",
+                        macros: {
+                            kcal: 675,
+                            proteins: 46.7,
+                            carbohydrates: 47.9,
+                            fats: 36.6
+                        },
+                        ingredients: [
+                            {
+                                name: "Jajko",
+                                count: 3,
+                                unit: "szt."
+                            },
+                            {
+                                name: "Kromki chleba pszennego",
+                                count: 4,
+                                unit: "szt."
+                            },
+                            {
+                                name: "masło roślinne",
+                                count: 200,
+                                unit: "g"
+                            },
+                        ],
+                        recipe: "Podgrzewaj na patelni 50g masła aż się roztopi, następnie wbij na patelnie 3 jajka, smarz aż źółtko i białko się zetnie. Posmaruj kromki chleba masłem."
+                    }
+                }
+            ]
+        },
+        {
+            meals: [
+                {
+                    name: "Śniadanie",
+                    meal: {
+                        name: "Jajecznica",
+                        macros: {
+                            kcal: 675,
+                            proteins: 46.7,
+                            carbohydrates: 47.9,
+                            fats: 36.6
+                        },
+                        ingredients: [
+                            {
+                                name: "Jajko",
+                                count: 3,
+                                unit: "szt."
+                            },
+                            {
+                                name: "Kromki chleba pszennego",
+                                count: 4,
+                                unit: "szt."
+                            },
+                            {
+                                name: "masło roślinne",
+                                count: 200,
+                                unit: "g"
+                            },
+                        ],
+                        recipe:
+                            "Podgrzewaj na patelni 50g masła aż się roztopi, następnie wbij na patelnie 3 jajka, smarz aż źółtko i białko się zetnie. Posmaruj kromki chleba masłem."
+                    }
+                }
+            ]
+        },
+        {
+            meals: [
+                {
+                    name: "Śniadanie",
+                    meal: {
+                        name: "Jajecznica",
+                        macros: {
+                            kcal: 675,
+                            proteins: 46.7,
+                            carbohydrates: 47.9,
+                            fats: 36.6
+                        },
+                        ingredients: [
+                            {
+                                name: "Jajko",
+                                count: 3,
+                                unit: "szt."
+                            },
+                            {
+                                name: "Kromki chleba pszennego",
+                                count: 4,
+                                unit: "szt."
+                            },
+                            {
+                                name: "masło roślinne",
+                                count: 200,
+                                unit: "g"
+                            },
+                        ],
+                        recipe:
+                            "Podgrzewaj na patelni 50g masła aż się roztopi, następnie wbij na patelnie 3 jajka, smarz aż źółtko i białko się zetnie. Posmaruj kromki chleba masłem."
+                    }
+                }
+            ]
+        },
+        {
+            meals: [
+                {
+                    name: "Śniadanie",
+                    meal: {
+                        name: "Jajecznica",
+                        macros: {
+                            kcal: 675,
+                            proteins: 46.7,
+                            carbohydrates: 47.9,
+                            fats: 36.6
+                        },
+                        ingredients: [
+                            {
+                                name: "Jajko",
+                                count: 3,
+                                unit: "szt."
+                            },
+                            {
+                                name: "Kromki chleba pszennego",
+                                count: 4,
+                                unit: "szt."
+                            },
+                            {
+                                name: "masło roślinne",
+                                count: 200,
+                                unit: "g"
+                            },
+                        ],
+                        recipe:
+                            "Podgrzewaj na patelni 50g masła aż się roztopi, następnie wbij na patelnie 3 jajka, smarz aż źółtko i białko się zetnie. Posmaruj kromki chleba masłem."
+                    }
+                },
+                {
+                    name: "Śniadanie",
+                    meal: {
+                        name: "Jajecznica",
+                        macros: {
+                            kcal: 675,
+                            proteins: 46.7,
+                            carbohydrates: 47.9,
+                            fats: 36.6
+                        },
+                        ingredients: [
+                            {
+                                name: "Jajko",
+                                count: 3,
+                                unit: "szt."
+                            },
+                            {
+                                name: "Kromki chleba pszennego",
+                                count: 4,
+                                unit: "szt."
+                            },
+                            {
+                                name: "masło roślinne",
+                                count: 200,
+                                unit: "g"
+                            },
+                        ],
+                        recipe:
+                            "Podgrzewaj na patelni 50g masła aż się roztopi, następnie wbij na patelnie 3 jajka, smarz aż źółtko i białko się zetnie. Posmaruj kromki chleba masłem."
+                    }
+                },
+                {
+                    name: "Śniadanie",
+                    meal: {
+                        name: "Jajecznica",
+                        macros: {
+                            kcal: 675,
+                            proteins: 46.7,
+                            carbohydrates: 47.9,
+                            fats: 36.6
+                        },
+                        ingredients: [
+                            {
+                                name: "Jajko",
+                                count: 3,
+                                unit: "szt."
+                            },
+                            {
+                                name: "Kromki chleba pszennego",
+                                count: 4,
+                                unit: "szt."
+                            },
+                            {
+                                name: "masło roślinne",
+                                count: 200,
+                                unit: "g"
+                            },
+                        ],
+                        recipe:
+                            "Podgrzewaj na patelni 50g masła aż się roztopi, następnie wbij na patelnie 3 jajka, smarz aż źółtko i białko się zetnie. Posmaruj kromki chleba masłem."
+                    }
+                }
+            ]
+        },
+        {
+            meals: [
+                {
+                    name: "Śniadanie",
+                    meal: {
+                        name: "Jajecznica",
+                        macros: {
+                            kcal: 675,
+                            proteins: 46.7,
+                            carbohydrates: 47.9,
+                            fats: 36.6
+                        },
+                        ingredients: [
+                            {
+                                name: "Jajko",
+                                count: 3,
+                                unit: "szt."
+                            },
+                            {
+                                name: "Kromki chleba pszennego",
+                                count: 4,
+                                unit: "szt."
+                            },
+                            {
+                                name: "masło roślinne",
+                                count: 200,
+                                unit: "g"
+                            },
+                        ],
+                        recipe:
+                            "Podgrzewaj na patelni 50g masła aż się roztopi, następnie wbij na patelnie 3 jajka, smarz aż źółtko i białko się zetnie. Posmaruj kromki chleba masłem."
+                    }
+                },
+                {
+                    name: "Śniadanie",
+                    meal: {
+                        name: "Jajecznica",
+                        macros: {
+                            kcal: 675,
+                            proteins: 46.7,
+                            carbohydrates: 47.9,
+                            fats: 36.6
+                        },
+                        ingredients: [
+                            {
+                                name: "Jajko",
+                                count: 3,
+                                unit: "szt."
+                            },
+                            {
+                                name: "Kromki chleba pszennego",
+                                count: 4,
+                                unit: "szt."
+                            },
+                            {
+                                name: "masło roślinne",
+                                count: 200,
+                                unit: "g"
+                            },
+                        ],
+                        recipe:
+                            "Podgrzewaj na patelni 50g masła aż się roztopi, następnie wbij na patelnie 3 jajka, smarz aż źółtko i białko się zetnie. Posmaruj kromki chleba masłem."
+                    }
+                }
+            ]
+        },
+        {
+            meals: [
+                {
+                    name: "Śniadanie",
+                    meal: {
+                        name:
+                            "Jajecznica",
+                        macros: {
+                            kcal: 675,
+                            proteins: 46.7,
+                            carbohydrates: 47.9,
+                            fats: 36.6
+                        },
+                        ingredients: [
+                            {
+                                name: "Jajko",
+                                count: 3,
+                                unit: "szt."
+                            },
+                            {
+                                name: "Kromki chleba pszennego",
+                                count: 4,
+                                unit: "szt."
+                            },
+                            {
+                                name: "masło roślinne",
+                                count: 200,
+                                unit: "g"
+                            },
+                        ],
+                        recipe:
+                            "Podgrzewaj na patelni 50g masła aż się roztopi, następnie wbij na patelnie 3 jajka, smarz aż źółtko i białko się zetnie. Posmaruj kromki chleba masłem."
+                    }
+
+                }
+            ]
+        },
+        {
+            meals: [
+                {
+                    name: "Śniadanie",
+                    meal: {
+                        name:
+                            "Jajecznica",
+                        macros: {
+                            kcal: 675,
+                            proteins: 46.7,
+                            carbohydrates: 47.9,
+                            fats: 36.6
+                        },
+                        ingredients: [
+                            {
+                                name: "Jajko",
+                                count: 3,
+                                unit: "szt."
+                            },
+                            {
+                                name: "Kromki chleba pszennego",
+                                count: 4,
+                                unit: "szt."
+                            },
+                            {
+                                name: "masło roślinne",
+                                count: 200,
+                                unit: "g"
+                            },
+                        ],
+                        recipe:
+                            "Podgrzewaj na patelni 50g masła aż się roztopi, następnie wbij na patelnie 3 jajka, smarz aż źółtko i białko się zetnie. Posmaruj kromki chleba masłem."
+                    }
+
+                }
+            ]
+        },
+    ]
+}
--- src/pages/DietPlanPage.jsx
+++ src/pages/DietPlanPage.jsx
@@ -1,7 +1,7 @@
 import NavigationBar from "../assets/elements/navigation/NavigationBar.jsx";
 import DietPlan from "../assets/elements/diet/DietPlan.jsx";
 import "../style/DietPlan.css"
-import {dietDayNames} from "../data/DietPlanData.js";
+import {dietDayNames, dietPlanData} from "../data/DietPlanData.js";
 
 function DietPlanPage() {
 
@@ -10,6 +10,7 @@ function DietPlanPage() {
             {<NavigationBar/>}
             <DietPlan
                 options={dietDayNames}
+                data={dietPlanData}
             />
         </div>
 )
