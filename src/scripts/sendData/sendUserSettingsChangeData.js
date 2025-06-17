0ef375b78437a773c503e55374a99c558a941aa1
JuliuszZolich <245971@edu.p.lodz.pl>
2025-06-17 14:22:47 +0200

    UserSetting fix

--- src/scripts/sendData/sendUserSettingsChangeData.js
+++ src/scripts/sendData/sendUserSettingsChangeData.js
@@ -1,23 +1,26 @@
+import {getDataFromLocalStorage} from "../getDataFromLocalStorage.js";
+
 export function sendChangeUserData(cookies, data){
-    const currentUserData = cookies['User-Data'];
+    const currentUserData = getDataFromLocalStorage("")
     currentUserData.email = data.userEmail
     currentUserData.phone = data.userPhone
     postNewData(cookies['User-Key'], currentUserData);
 }
 
 export function sendChangeUserPassword(cookies, data){
-    const currentUserData = cookies['User-Data'];
+    const currentUserData = getDataFromLocalStorage("")
     currentUserData.password = data.userNewPassword
     postNewData(cookies['User-Key'], currentUserData);
 }
 
 export function sendChangeUserDescription(cookies, data){
-    const currentUserData = cookies['User-Data'];
-    currentUserData.description = data.userDescription
+    const currentUserData = getDataFromLocalStorage("")
+    currentUserData.description = data
     postNewData(cookies['User-Key'], currentUserData);
 }
 
 function postNewData(userKey, data){
+    console.log(data)
     fetch("/api/update/user", {
         method: "POST",
         headers: {
